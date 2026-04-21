81c18e3a532826b3edd0a0100f6bd18e94e3057f
Dimi <demsb48+git@gmail.com>
2026-04-22 00:12:41 +0100

    testing new bootstrap.sh 5

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,6 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-read -rp "Github username: " GH_USER
-read -rp "Github repo name: " GH_REPO
-
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PAT_FILE="$SCRIPT_DIR"/github_pat.age
 
@@ -18,4 +15,4 @@ git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https:/
 
 sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
 
-chezmoi init --apply "$GH_USER/$GH_REPO"
+chezmoi init --apply "unkythira/dotfiles"
